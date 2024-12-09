b55e9bf50040da1974194cc8c5ab8237d0ba1836
Max <makimov2004@gmail.com>
2024-12-09 22:03:58 +0200

    Carvires2

--- app/main.py
+++ app/main.py
@@ -1,21 +1,30 @@
-class Animal:
-    alive = []
+from __future__ import annotations
 
-    def __init__(self, name: str, health: int = 100,
-                 hidden: bool = False) -> None:
+
+class Animal:
+    alive: list[Animal] = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
-        Animal.alive.append(self)
+        if self.health > 0:
+            Animal.alive.append(self)
 
     def die(self) -> None:
-        if self in Animal.alive:
-            Animal.alive.remove(self)
+        if self.health <= 0:
+            if self in Animal.alive:
+                Animal.alive.remove(self)
 
-    def __repr__(self) -> None:
-        return (f"{{Name: {self.name}"
-                f", Health: {self.health}"
-                f", Hidden: {self.hidden}}}")
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -24,8 +33,8 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore) -> None:
-        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
-            herbivore.health -= 50
-            if herbivore.health <= 0:
-                herbivore.die()
+    def bite(self, target: Animal) -> None:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
+            if target.health <= 0:
+                target.die()
